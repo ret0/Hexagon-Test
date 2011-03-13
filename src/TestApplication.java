439f89851724a138769840813fbe785bfce39cd8
Reto Kleeb <retokl@gmail.com>
2011-03-12 19:32:59 -0500

    cleanup

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -1,102 +1,72 @@
-import java.awt.*;
-import java.awt.geom.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Shape;
 
-import sl.shapes.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import sl.shapes.RegularPolygon;
 
 public class TestApplication extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
     Canvas cPolygons = new Canvas();
-    Canvas cStars = new Canvas();
-    Canvas cRoundPolygons = new Canvas();
+
     public TestApplication() {
         super("Shapes demo");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(new GridBagLayout());
 
-        getContentPane().add(new JLabel("Regular polygons"),
-                             new GridBagConstraints(0, 0, 1, 1, 1, 0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 0, 5), 0, 0));
-        getContentPane().add(cPolygons, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(5, 5, 0, 5), 0, 0));
-
-        getContentPane().add(new JLabel("Star polygons"),
-                             new GridBagConstraints(0, 2, 1, 1, 1, 0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 0, 5), 0, 0));
-        getContentPane().add(cStars, new GridBagConstraints(0, 3, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(5, 5, 0, 5), 0, 0));
-
-        getContentPane().add(new JLabel("Round polygons"),
-                             new GridBagConstraints(0, 4, 1, 1, 1, 0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 0, 5), 0, 0));
-        getContentPane().add(cRoundPolygons, new GridBagConstraints(0, 5, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0));
+        getContentPane().add(
+                cPolygons,
+                new GridBagConstraints(0, 1, 1, 1, 1, 1,
+                        GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH,
+                        new Insets(5, 5, 0, 5), 0, 0));
 
         initRegular();
-        initStars();
-        initRoundPolygon();
 
         setSize(800, 600);
         setLocationRelativeTo(null);
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         TestApplication fr = new TestApplication();
         fr.setVisible(true);
     }
 
     protected void initRegular() {
-        Shape[] shapes = new Shape[7];
-        shapes[0] = new RegularPolygon(50, 50, 50, 3, Math.PI / 4);
-        shapes[1] = new RegularPolygon(150, 100, 50, 4, 0);
-        shapes[2] = new RegularPolygon(250, 50, 50, 5, Math.PI / 4);
-        shapes[3] = new RegularPolygon(350, 100, 50, 6, 0);
-        shapes[4] = new RegularPolygon(450, 50, 50, 7, Math.PI / 4);
-        shapes[5] = new RegularPolygon(550, 100, 50, 8, 0);
-        shapes[6] = new RegularPolygon(650, 50, 50, 9, Math.PI / 4);
-
+        Shape[] shapes = new Shape[1];
+        shapes[0] = new RegularPolygon(350, 100, 50, 6, 0);
         cPolygons.setShapes(shapes, Color.blue);
     }
 
-    protected void initRoundPolygon() {
-        Shape[] shapes = new Shape[7];
-        shapes[0] = new RoundPolygon(new RegularPolygon(50, 50, 50, 3, Math.PI / 4), 10);
-        shapes[1] = new RoundPolygon(new RegularPolygon(150, 100, 50, 4, 0), 15);
-        shapes[2] = new RoundPolygon(new StarPolygon(250, 50, 50, 20, 5, Math.PI / 4), 7);
-        shapes[3] = new RoundPolygon(new Polygon(new int[] {300, 350, 400, 350}, new int[] {50, 50, 150, 150}, 4), 10);
-        shapes[4] = new RoundPolygon(new RegularPolygon(450, 50, 50, 7, Math.PI / 4), 10);
-        shapes[5] = new RoundPolygon(new StarPolygon(550, 100, 50, 20, 8, 0), 10);
-        shapes[6] = new RoundPolygon(new StarPolygon(650, 50, 50, 30, 9, Math.PI / 4), 10);
-
-        cRoundPolygons.setShapes(shapes, Color.green);
-    }
-
-    protected void initStars() {
-        Shape[] shapes = new Shape[7];
-        shapes[0] = new StarPolygon(50, 50, 50, 10, 3, Math.PI / 4);
-        shapes[1] = new StarPolygon(150, 100, 50, 20, 4, 0);
-        shapes[2] = new StarPolygon(250, 50, 50, 20, 5, Math.PI / 4);
-        shapes[3] = new StarPolygon(350, 100, 50, 20, 6, 0);
-        shapes[4] = new StarPolygon(450, 50, 50, 30, 7, Math.PI / 4);
-        shapes[5] = new StarPolygon(550, 100, 50, 20, 8, 0);
-        shapes[6] = new StarPolygon(650, 50, 50, 30, 9, Math.PI / 4);
-
-        cStars.setShapes(shapes, Color.red);
-    }
-
     protected static class Canvas extends JPanel {
+        private static final long serialVersionUID = 1L;
+
         Shape[] shapes;
         Color color;
-        public void setShapes(Shape[] shapes, Color color) {
+
+        public void setShapes(final Shape[] shapes, final Color color) {
             this.shapes = shapes;
             this.color = color;
         }
 
-        public void paintComponent(Graphics g) {
+        @Override
+        public void paintComponent(final Graphics g) {
             super.paintComponent(g);
             g.setColor(Color.white);
             g.fillRect(0, 0, getWidth(), getHeight());
             g.setColor(Color.black);
             g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
             g.setColor(color);
-            for (int i = 0; i < shapes.length; i++) {
-                ( (Graphics2D) g).draw(shapes[i]);
-
+            for (Shape shape2 : shapes) {
+                ((Graphics2D) g).draw(shape2);
             }
-            RoundRectangle2D.Double rd2;
         }
     }
 }
--- src/sl/shapes/RegularPolygon.java
+++ src/sl/shapes/RegularPolygon.java
@@ -1,35 +1,37 @@
 package sl.shapes;
 
-import java.awt.*;
+import java.awt.Polygon;
 
 public class RegularPolygon extends Polygon {
-    public RegularPolygon(int x, int y, int r, int vertexCount) {
-        this(x, y, r, vertexCount, 0);
-    }
-    public RegularPolygon(int x, int y, int r, int vertexCount, double startAngle) {
-        super(getXCoordinates(x, y, r, vertexCount, startAngle)
-              ,getYCoordinates(x, y, r, vertexCount, startAngle)
-              ,vertexCount);
+
+    private static final long serialVersionUID = 1L;
+
+    public RegularPolygon(final int x, final int y, final int r, final int vertexCount,
+            final double startAngle) {
+        super(getXCoordinates(x, r, vertexCount, startAngle),
+                getYCoordinates(y, r, vertexCount, startAngle), vertexCount);
     }
 
-    protected static int[] getXCoordinates(int x, int y, int r, int vertexCount, double startAngle) {
-        int res[]=new int[vertexCount];
-        double addAngle=2*Math.PI/vertexCount;
-        double angle=startAngle;
-        for (int i=0; i<vertexCount; i++) {
-            res[i]=(int)Math.round(r*Math.cos(angle))+x;
-            angle+=addAngle;
+    protected static int[] getXCoordinates(final int x, final int r,
+            final int vertexCount, final double startAngle) {
+        int res[] = new int[vertexCount];
+        double addAngle = 2 * Math.PI / vertexCount;
+        double angle = startAngle;
+        for (int i = 0; i < vertexCount; i++) {
+            res[i] = (int) Math.round(r * Math.cos(angle)) + x;
+            angle += addAngle;
         }
         return res;
     }
 
-    protected static int[] getYCoordinates(int x, int y, int r, int vertexCount, double startAngle) {
-        int res[]=new int[vertexCount];
-        double addAngle=2*Math.PI/vertexCount;
-        double angle=startAngle;
-        for (int i=0; i<vertexCount; i++) {
-            res[i]=(int)Math.round(r*Math.sin(angle))+y;
-            angle+=addAngle;
+    protected static int[] getYCoordinates(final int y, final int r,
+            final int vertexCount, final double startAngle) {
+        int res[] = new int[vertexCount];
+        double addAngle = 2 * Math.PI / vertexCount;
+        double angle = startAngle;
+        for (int i = 0; i < vertexCount; i++) {
+            res[i] = (int) Math.round(r * Math.sin(angle)) + y;
+            angle += addAngle;
         }
         return res;
     }
